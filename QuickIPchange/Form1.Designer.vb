883f91cd4eaf7531641a8c9a8049dbbfc90768a0
larshagrid <interfacegui@gmail.com>
2023-08-14 09:59:12 +0800

    加入專案檔案。

---
+++ QuickIPchange/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Font = New System.Drawing.Font("Microsoft JhengHei UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(12, 46)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(435, 47)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "設定為DCHP"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Font = New System.Drawing.Font("Microsoft JhengHei UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(12, 99)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(435, 47)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "設定為 192.168.1.66"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button3.Font = New System.Drawing.Font("Microsoft JhengHei UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.Location = New System.Drawing.Point(12, 152)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(435, 47)
+        Me.Button3.TabIndex = 0
+        Me.Button3.Text = "設定為 ABB"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button4.Font = New System.Drawing.Font("Microsoft JhengHei UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button4.Location = New System.Drawing.Point(12, 205)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(435, 47)
+        Me.Button4.TabIndex = 0
+        Me.Button4.Text = "設定為 192.168.0.66"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button5.Font = New System.Drawing.Font("Microsoft JhengHei UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button5.Location = New System.Drawing.Point(12, 258)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(435, 47)
+        Me.Button5.TabIndex = 0
+        Me.Button5.Text = "設定為192.168.100.66"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(12, 311)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(435, 169)
+        Me.ListBox1.TabIndex = 1
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft JhengHei UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(435, 28)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(459, 492)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "快速IP更改"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ QuickIPchange/Form1.vb
@@ -0,0 +1,221 @@
+﻿Imports System.Net.NetworkInformation
+Imports System.Security.Principal
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Private Sub getinterfaces()
+        Dim nics As NetworkInterface() = NetworkInterface.GetAllNetworkInterfaces
+        If nics.Length < 0 Or nics Is Nothing Then
+            MsgBox("No NICS")
+            Exit Sub
+        End If
+        ComboBox1.Items.Clear()
+
+        For Each netadapter As NetworkInterface In nics
+            Dim intproperties As IPInterfaceProperties = netadapter.GetIPProperties()
+            ComboBox1.Items.Add(netadapter.Name)
+        Next
+        GetActiveNetworkInterfaceName()
+    End Sub
+
+    Private Sub ComboBox1_DropDown(sender As Object, e As EventArgs) Handles ComboBox1.DropDown
+
+        getinterfaces()
+    End Sub
+
+
+    Private Sub ChangeIPAddress(interfaceName As String, agrs As String
+                                )
+        Dim processStartInfo As New ProcessStartInfo()
+        processStartInfo.FileName = "netsh"
+        processStartInfo.Arguments = $"interface ip set address ""{interfaceName}"" {agrs}"
+        processStartInfo.RedirectStandardOutput = True
+        processStartInfo.UseShellExecute = False
+        processStartInfo.CreateNoWindow = True
+
+        Dim process As New Process()
+        process.StartInfo = processStartInfo
+        process.Start()
+        AddHandler process.OutputDataReceived, Sub()
+                                                   AddToOutputListBox(process.StandardOutput.ReadToEnd())
+                                               End Sub
+        'Dim output As String = process.StandardOutput
+        process.WaitForExit()
+
+        If process.ExitCode = 0 Then
+            AddToOutputListBox("IP address and subnet mask changed successfully.")
+        Else
+            AddToOutputListBox($"An error occurred")
+        End If
+        GetInterfaceInfo(interfaceName)
+    End Sub
+
+    Private Sub renewDHCP(interfaceName As String, agrs As String)
+        Dim processStartInfo As New ProcessStartInfo()
+        processStartInfo.FileName = "netsh"
+        processStartInfo.Arguments = $"{agrs}"
+        processStartInfo.RedirectStandardOutput = True
+        processStartInfo.UseShellExecute = False
+        processStartInfo.CreateNoWindow = True
+
+        Dim process As New Process()
+        process.StartInfo = processStartInfo
+        process.Start()
+        AddHandler process.OutputDataReceived, Sub()
+                                                   AddToOutputListBox(process.StandardOutput.ReadToEnd())
+                                               End Sub
+        'Dim output As String = process.StandardOutput
+        process.WaitForExit()
+
+        If process.ExitCode = 0 Then
+            AddToOutputListBox("")
+        Else
+            AddToOutputListBox($"An error occurred")
+        End If
+
+        GetInterfaceInfo(interfaceName)
+    End Sub
+
+    Private Sub AddToOutputListBox(message As String)
+        ListBox1.Items.Add(message)
+    End Sub
+
+    Private Sub GetInterfaceInfo(interfaceName As String)
+        Dim processStartInfo As New ProcessStartInfo()
+        processStartInfo.FileName = "ipconfig"
+        processStartInfo.Arguments = ""
+        processStartInfo.RedirectStandardOutput = True
+        processStartInfo.UseShellExecute = False
+        'processStartInfo.StandardOutputEncoding = System.Text.Encoding.GetEncoding("GBK") ' Set the appropriate encoding for Chinese characters
+        processStartInfo.CreateNoWindow = True
+
+        Dim process As New Process()
+        process.StartInfo = processStartInfo
+        process.Start()
+
+        Dim output As String = process.StandardOutput.ReadToEnd()
+        process.WaitForExit()
+
+        Dim interfaceInfoPattern As String = $"({interfaceName}).+?IPv4 位址[ .]+: (.+?)\r?\n.+?子網路遮罩[ .]+: (.+?)\r?\n.+?預設閘道[ .]+: (.+?)\r?\n"
+        Dim match As Match = Regex.Match(output, interfaceInfoPattern, RegexOptions.Singleline)
+
+        If match.Success Then
+            Dim ipAddress As String = match.Groups(2).Value
+            Dim subnetMask As String = match.Groups(3).Value
+            Dim defaultGateway As String = match.Groups(4).Value
+
+            AddMessageToListBox($"介面卡: {interfaceName}")
+            AddMessageToListBox($"IPv4 位址: {ipAddress}")
+            AddMessageToListBox($"子網路遮罩: {subnetMask}")
+            AddMessageToListBox($"預設閘道: {defaultGateway}")
+        Else
+            AddMessageToListBox($"未找到接口 '{interfaceName}' 信息。")
+        End If
+    End Sub
+
+    Private Sub AddMessageToListBox(message As String)
+        ListBox1.Items.Add(message)
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ChangeIPAddress(ComboBox1.SelectedItem, "static 192.168.1.66 255.255.255.0 192.168.1.1")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ChangeIPAddress(ComboBox1.SelectedItem, "static 192.168.100.66 255.255.255.0 192.168.100.1")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ChangeIPAddress(ComboBox1.SelectedItem, "static 192.168.125.199 255.255.255.0 192.168.125.1")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ChangeIPAddress(ComboBox1.SelectedItem, "source = dhcp")
+        renewDHCP(ComboBox1.SelectedItem, "/release")
+        renewDHCP(ComboBox1.SelectedItem, "/renew")
+        'GetInterfaceInfo(ComboBox1.SelectedItem)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ChangeIPAddress(ComboBox1.SelectedItem, "static 192.168.0.66 255.255.255.0 192.168.0.1")
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        ListBox1.Items.Clear()
+        GetInterfaceInfo(ComboBox1.SelectedItem)
+    End Sub
+
+    Private Sub GetActiveNetworkInterfaceName()
+        Dim activeNetworkInterface As NetworkInterface = GetActiveNetworkInterface()
+
+        If activeNetworkInterface IsNot Nothing Then
+            Dim interfaceName As String = activeNetworkInterface.Name
+            ComboBox1.SelectedItem = $"{interfaceName}"
+            AddMessageToListBox1($"正在使用的網卡名稱: {interfaceName}")
+        Else
+            AddMessageToListBox1("未找到正在使用的網卡。")
+        End If
+    End Sub
+
+    Private Function GetActiveNetworkInterface() As NetworkInterface
+        Dim networkInterfaces As NetworkInterface() = NetworkInterface.GetAllNetworkInterfaces()
+
+        For Each networkInterface As NetworkInterface In networkInterfaces
+            If networkInterface.OperationalStatus = OperationalStatus.Up Then
+                'AndAlso networkInterface.NetworkInterfaceType <> NetworkInterfaceType.Loopback Then
+                Return networkInterface
+            End If
+        Next
+
+        Return Nothing
+    End Function
+
+    Private Sub AddMessageToListBox1(message As String)
+        ListBox1.Items.Add(message)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If Not IsAdministrator() Then
+            RestartAsAdministrator()
+        End If
+
+
+        getinterfaces()
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If ComboBox1.SelectedItem = "" Then Return
+        ListBox1.Items.Clear()
+        GetInterfaceInfo(ComboBox1.SelectedItem)
+
+    End Sub
+
+
+    Private Function IsAdministrator() As Boolean
+        Dim identity As WindowsIdentity = WindowsIdentity.GetCurrent()
+        Dim principal As WindowsPrincipal = New WindowsPrincipal(identity)
+        Return principal.IsInRole(WindowsBuiltInRole.Administrator)
+    End Function
+
+    Private Sub RestartAsAdministrator()
+        Dim startInfo As ProcessStartInfo = New ProcessStartInfo()
+        startInfo.FileName = Application.ExecutablePath
+        startInfo.UseShellExecute = True
+        startInfo.Verb = "runas" ' This indicates running as administrator
+        Try
+            Dim process As Process = New Process()
+            process.StartInfo = startInfo
+            process.Start()
+            Application.Exit()
+        Catch ex As Exception
+            ' Handle the exception if elevation fails
+        End Try
+    End Sub
+
+
+
+End Class
